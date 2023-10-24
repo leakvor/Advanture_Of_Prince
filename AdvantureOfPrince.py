d60f1cd07ec039a93dd099d06cfef8053b9baa35
VORLEAK.KUN <VORLEAK.KUN@PNC.LAN>
2023-10-24 19:52:25 +0700

    #2 update image and import to python

--- AdvantureOfPrince.py
+++ AdvantureOfPrince.py
@@ -1,7 +1,7 @@
 #============================ IMPORTS ============================
 import tkinter as tk
 from tkinter import *
-# from PIL import Image, ImageTk
+from PIL import Image, ImageTk
 from pygame import mixer
 import time
 # from random import randrange
@@ -9,10 +9,6 @@ import random
 # ---------------------------------------------------------------------------
 #=> CONSTANT
 # ---------------------------------------------------------------------------
-root = tk.Tk()
-root.geometry(str(SCREEN_WIDTH)+"x"+str(SCREEN_HEIGHT))
-root.title('Advanture OF Prince')
-canvas = tk.Canvas(root)
 
 SCREEN_WIDTH = 1400
 SCREEN_HEIGHT = 740
@@ -33,9 +29,21 @@ fire_score=10
 isWin=False
 isLose=False
 
+# ---------------------------------------------------------------------------
+root = tk.Tk()
+root.geometry(str(SCREEN_WIDTH)+"x"+str(SCREEN_HEIGHT))
+root.title('Advanture OF Prince')
+canvas = tk.Canvas(root)
+
+# ---------------------------------------------------------------------------
+#=> Global
+# player
+# ---------------------------------------------------------------------------
+#=> VARIABLES
+# ---------------------------------------------------------------------------
 game_start=tk.PhotoImage(file='image/bg.png')
-game_win=tk.PhotoImage(file='image/Gameover.png')
-game_over=tk.PhotoImage(file='image/Gamewin.png')
+game_over=tk.PhotoImage(file='image/Gameover.png')
+game_win=tk.PhotoImage(file='image/Gamewin.png')
 game_introduction=tk.PhotoImage(file='image/introduction-of-game.png')
 game_story=tk.PhotoImage(file='image/story.png')
 
@@ -53,9 +61,10 @@ story_list=tk.PhotoImage(file='image/story-list.png')
 slide1=tk.PhotoImage(file='image/slide1.png')
 slide2=tk.PhotoImage(file='image/slide2.png')
 slide3=tk.PhotoImage(file='image/slide3.png')
-grass=tk.PhotoImage(file='image/grass.png')
+
 
 hero=tk.PhotoImage(file='image/Ranger.png')
+flower=tk.PhotoImage(file='image/flower.png')
 queen=tk.PhotoImage(file='image/queen.png')
 coin=tk.PhotoImage(file='image/coin.png')
 water=tk.PhotoImage(file='image/water.png')
@@ -65,11 +74,12 @@ long_wall=tk.PhotoImage(file='image/long-wall.png')
 monster=tk.PhotoImage(file='image/monster.png')
 fire=tk.PhotoImage(file='image/fire.png')
 all_level_bg= PhotoImage(file="image/all-levels.png")
-
-# ---------------------------------------------------------------------------
-
-
-
+score_id = canvas.create_text(600, 15, text="Score:", font=("bold", 15))
+button_exists= PhotoImage(file="image/button_exist.png")
+button_level=tk.PhotoImage(file='image/button_level.png')
+play_again=tk.PhotoImage(file='image/playAgain.png')
+retry=tk.PhotoImage(file='image/Retry.png')
+back_to_game=tk.PhotoImage(file='image/back.png')
 
 
 # ---------------------------------------------------------------------------
