5d8936139fd46c9075a2e0e44b92bb252c71a889
VORLEAK.KUN <VORLEAK.KUN@pnc.lan>
2023-10-26 19:43:12 +0700

    delete global not event

--- AdvantureOfPrince.py
+++ AdvantureOfPrince.py
@@ -20,14 +20,12 @@ TIMED_LOOP = 10
 #=> Global
 # ---------------------------------------------------------------------------
 score=0
-notEvent=False
 hight_score=0
 keyPressed = []
 coin_score=4
 water_score=7
 fire_score=10
-isWin=False
-isLose=False
+
 
 # ---------------------------------------------------------------------------
 root = tk.Tk()
@@ -35,9 +33,6 @@ root.geometry(str(SCREEN_WIDTH)+"x"+str(SCREEN_HEIGHT))
 root.title('Advanture OF Prince')
 canvas = tk.Canvas(root)
 
-# ---------------------------------------------------------------------------
-#=> Global
-# player
 # ---------------------------------------------------------------------------
 #=> VARIABLES
 # ---------------------------------------------------------------------------
